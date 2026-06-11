041713b578ff21ca2377c3ffb0e9290f2528f618
Mark Danello <mdanello26@gmail.com>
2026-06-11 13:00:08 -0400

    Display initial catalog component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CatalogComponent } from "./catalog/catalog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CatalogComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
