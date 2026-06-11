1e305074e838c134650fbc274f9f714c8690c6e1
Mark Danello <mdanello26@gmail.com>
2026-06-11 14:36:16 -0400

    move product details to component

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductDetailsComponent } from "../product-details/product-details.component";
 
 @Component({
   selector: 'bot-catalog',
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
 })
