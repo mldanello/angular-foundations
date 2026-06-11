4ea13733ba08d6ae119e9e235b6a10b93357df7a
Mark Danello <mdanello26@gmail.com>
2026-06-11 13:05:15 -0400

    set default component prefix

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-catalog',
+  selector: 'bot-catalog',
   imports: [],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
