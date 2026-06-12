449886587bf624b048da21f8e2be174d28f9ec6c
Mark Danello <mdanello26@gmail.com>
2026-06-12 15:44:12 -0400

    databinding - interpolation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'bot-product-details',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
-
+  product : IProduct;
+  
+  constructor() {
+    this.product = {
+      id: 2,
+      description: "A friendly robot head with two eyes and a smile -- great for domestic use!",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.99,
+      discount: 0.2
+    }
+  }
 }
